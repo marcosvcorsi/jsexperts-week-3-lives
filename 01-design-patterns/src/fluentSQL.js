05bc2399f33852e8302bd7e2d7a355e2fbc8da79
Marcos Corsi <marcosvcorsi@gmail.com>
2021-05-22 22:54:20 -0300

    feat: add first class TDD structure

--- 01-design-patterns/src/fluentSQL.js
+++ 01-design-patterns/src/fluentSQL.js
@@ -1,5 +1,8 @@
 export class FluentSQLBuilder {
   #database = [];
+  #select = [];
+  #limit = 0;
+  #where = [];
 
   constructor({ database }) {
     this.#database = database;
@@ -7,5 +10,29 @@ export class FluentSQLBuilder {
 
   static for(database) {
     return new FluentSQLBuilder({ database })
+  }
+
+  select(props) {
+    this.#select = props;
+
+    return this;
+  }
+
+  limit(max) {
+    this.#limit = max;
+
+    return this;
+  }
+
+  where(query) {
+    this.#where = query;
+
+    return this;
+  }
+
+  build() {
+    const results = [];
+
+    return results;
   }
 }
--- 01-design-patterns/test/fluentSQL.test.js
+++ 01-design-patterns/test/fluentSQL.test.js
@@ -1,5 +1,54 @@
+import { FluentSQLBuilder } from "../src/fluentSQL";
+
+const data = [
+  {
+    id: 0,
+    name: 'anyname',
+    category: 'anycategory'
+  },
+  {
+    id: 1,
+    name: 'othername',
+    category: 'othercategory'
+  },
+  {
+    id: 2,
+    name: 'name',
+    category: 'othercategory'
+  },
+]
+
 describe('Test suite for FluentSQL Builder', () => {
-  it('should be true', () => {
-    expect(1).toBe(1);
+  it('should return a FluentSQLBuilder instance', () => {
+    const result = FluentSQLBuilder.for(data);
+
+    expect(result).toBeInstanceOf(FluentSQLBuilder);
+    expect(result).toEqual(new FluentSQLBuilder({ database: data }));
+  })
+
+  it('should return the empty array instance', () => {
+    const result = FluentSQLBuilder.for(data).build();
+
+    expect(result).toEqual([]);
+  })
+
+  it('should given a collection with limit results', () => {
+
+  })
+
+  it('should filter data with where', () => {
+    
+  })
+
+  it('should return only specific fields', () => {
+
+  })
+
+  it('should return order results by specific fields', () => {
+    
+  })
+
+  it('should combine operations', () => {
+
   })
 });
