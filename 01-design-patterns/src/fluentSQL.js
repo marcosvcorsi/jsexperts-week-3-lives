d3e383d24cfbfac854efc927662db8819b231f33
Marcos Corsi <marcosvcorsi@gmail.com>
2021-05-23 20:57:37 -0300

    feat: add where and limit to fluent sql

--- 01-design-patterns/src/fluentSQL.js
+++ 01-design-patterns/src/fluentSQL.js
@@ -3,6 +3,7 @@ export class FluentSQLBuilder {
   #select = [];
   #limit = 0;
   #where = [];
+  #orderBy = '';
 
   constructor({ database }) {
     this.#database = database;
@@ -25,13 +26,55 @@ export class FluentSQLBuilder {
   }
 
   where(query) {
-    this.#where = query;
+    const conditions = Object.entries(query).map(([prop, selectedValue]) => {
+      const filter = selectedValue instanceof RegExp ? 
+        selectedValue : new RegExp(selectedValue);
+      
+      return {
+        prop,
+        filter
+      }
+    })
+
+    for(const condition of conditions) {
+      this.#where.push(condition);
+    }
 
     return this;
   }
 
+  orderBy(field) {
+    this.#orderBy = field;
+
+    return this;
+  }
+
+  #performLimit(results) {
+    return this.#limit && results.length === this.#limit;
+  }
+
+  #performWhere(item) {
+    for(const { prop, filter } of this.#where) {
+      if(!filter.test(item[prop])) return false;
+    }
+
+    return true;
+  }
+
   build() {
     const results = [];
+
+    for(const item of this.#database) {
+      if(!this.#performWhere(item)) {
+        continue;
+      }
+
+      results.push(item);
+
+      if(this.#performLimit(results)) {
+        break;
+      }
+    }
 
     return results;
   }
--- 01-design-patterns/test/fluentSQL.test.js
+++ 01-design-patterns/test/fluentSQL.test.js
@@ -14,7 +14,7 @@ const data = [
   {
     id: 2,
     name: 'name',
-    category: 'othercategory'
+    category: 'anycategory'
   },
 ]
 
@@ -27,17 +27,30 @@ describe('Test suite for FluentSQL Builder', () => {
   })
 
   it('should return the empty array instance', () => {
-    const result = FluentSQLBuilder.for(data).build();
+    const result = FluentSQLBuilder.for([]).build();
 
     expect(result).toEqual([]);
   })
 
   it('should given a collection with limit results', () => {
+    const limit = 1;
 
+    const result = FluentSQLBuilder.for(data).limit(1).build();
+
+    expect(result).toHaveLength(limit);
+    expect(result).toEqual([data[0]]);
   })
 
   it('should filter data with where', () => {
-    
+    const query =  {
+      category: /^any/
+    };
+
+    const expected = data.filter(({ category }) => category.slice(0,3) === 'any')
+
+    const result = FluentSQLBuilder.for(data).where(query).build();
+
+    expect(result).toEqual(expected);
   })
 
   it('should return only specific fields', () => {
